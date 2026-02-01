b625e7d1c120192f26cbfcd0bac2d1c81462121f
Adib-mv <adib.mv@gmail.com>
2026-02-01 13:50:18 +0300

    Update Lesson_2.java

--- Lesson_2.java
+++ Lesson_2.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Lesson_2 {
 
 
@@ -10,6 +12,12 @@ public class Lesson_2 {
             System.out.println(checkSumInRange(11,9));
             checkNumberSign(11);
             System.out.println(NegativeNumberChecker(-11));
+            printStringMultipleTimes("Hello!",3);
+	        System.out.println(checkYear(2024));
+	        BinaryArrayConverter();
+	        FillArray();
+ 	        SimpleSolution();
+	        SimpleDiagon();
 
 
         }
@@ -92,6 +100,98 @@ public class Lesson_2 {
             }
 
     }
+    {
+        printStringMultipleTimes("Hello!",3);
+    }
+
+    public static void printStringMultipleTimes(String text, int count) {
+
+        if (count <= 0) {
+
+            return;
+        }
+        for (int i = 0; i < count; i++) {
+        }
+    }
+
+{
+    System.out.println(checkYear(2024));
+}
+    public static boolean checkYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else if (year % 4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+{
+        BinaryArrayConverter();
+    }
+    public static void BinaryArrayConverter() {
+        int[] array = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 0) {
+                array[i] = 1;
+            } else {
+                array[i] = 0;
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+
+{
+             FillArray();
+        }
+    public static void FillArray() {
+            int[] arr = new int[100];
+            for (int i = 0; i < 100; i++) {
+            arr[i] = i;
+    }
+    System.out.println(Arrays.toString(arr));
+
+        }
+{
+            SimpleSolution();
+        }
+        public static void SimpleSolution() {
+            int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i] < 6) {
+                    arr[i] = arr[i] * 2;
+                }
+            }
+
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+        }
+{
+            SimpleDiagon();
+        }
+            public static void SimpleDiagon() {
+
+            int n = 5;
+            int[][] arr = new int[n][n];
+
+
+            for (int i = 0; i < n; i++) {
+                arr[i][i] = 1;
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.print(arr[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
 
     }
 
