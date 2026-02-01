942602c388f89816eebef6a3868a35b9557d7910
Adib-mv <adib.mv@gmail.com>
2026-02-01 10:16:24 +0300

    Add files via upload

---
+++ Lesson_2.java
@@ -0,0 +1,97 @@
+public class Lesson_2 {
+
+
+        public static void main(String[] args) {
+
+            printThreeWords();
+            checkSumSign();
+            printColor();
+            compareNumbers();
+            System.out.println(checkSumInRange(11,9));
+            checkNumberSign(11);
+            System.out.println(NegativeNumberChecker(-11));
+
+
+        }
+
+        public static void printThreeWords() {
+            System.out.println("Orange");
+            System.out.println("Banana");
+            System.out.println("Apple");
+       
+        }
+
+    public static void checkSumSign() {
+        int a = 5;
+        int b = 8;
+
+        int sum = a + b;
+
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor() {
+
+        int value = 118;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        else if (value > 0 && value <= 100) {
+            System.out.println("Желтый");
+        }
+        else {
+            System.out.println("Зеленый");
+        }
+    }
+
+    public static void compareNumbers() {
+
+        int a = 15;
+        int b = 100;
+
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+          public static boolean checkSumInRange(int a, int b) {
+
+        int sum = a + b;
+
+        if (sum >= 10 && sum <= 20) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+        public static void checkNumberSign(int a) {
+
+        int b = a;
+
+            if (b >= 0) {
+                System.out.println(a + " - положительное число");
+            } else {
+                System.out.println(a + " - отрицательное число");
+            }
+        }
+
+	public static boolean NegativeNumberChecker(int a) {
+        int b = a;
+
+            if (b < 0) {
+                return true;
+            } else {
+                return false;
+            }
+
+    }
+
+    }
+
